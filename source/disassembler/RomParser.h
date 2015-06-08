2320dc2d94061f06262aa43c94de191c5eb27a41
josephstill <joseph.walker.tmp@gmail.com>
2015-06-07 22:39:10 -0400

    Added documentation

--- source/disassembler/RomParser.h
+++ source/disassembler/RomParser.h
@@ -8,19 +8,33 @@ namespace disassembler
 {
 
 /**
- * Parses ROM files and provides a list of instructions.
+ * This class provides a set of static utilities relating to reading a ROM file.
  */
 class RomParser
 {
 
 public:
 
+	/**
+	 * A container that holds a single command
+	 */
 	struct command
 	{
 		unsigned char val[2];
 	};
 
+	/**
+	 * Parses the ROM file into individual commands and provides the commands in a vector.
+	 * @param romFileName The path to the file to parse
+	 * @return a vector of commands
+	 */
 	static std::vector<RomParser::command> parseRom(std::string romFileName);
+
+	/**
+	 * Creates a hex dump of the provided ROM file.
+	 * @param romFileName The path to the file to parse.
+	 * @return a string containing the hex dump.
+	 */
 	static std::string hexDump(std::string romFileName);
 
 private:
